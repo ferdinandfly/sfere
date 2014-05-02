7a34d16b93629c90a521224dc04353f2f8af0879
ferdinandfly <ferdinandfly@ferdinandfly--pc.(none)>
2014-05-02 23:22:29 +0200

    package problem

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,7 +30,7 @@ class AppKernel extends Kernel
             new Sonata\CoreBundle\SonataCoreBundle(),
             new Sonata\BlockBundle\SonataBlockBundle(),
             new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
-            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            //new Sonata\jQueryBundle\SonatajQueryBundle(),
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
             new A2lix\TranslationFormBundle\A2lixTranslationFormBundle(),
             new Ardetem\SfereBundle\ArdetemSfereBundle(),
