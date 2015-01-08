9b4072a61d09db4694c3980fa4dc1f68335c7b3e
Hao <h.tan@kreactive.com>
2015-01-08 15:28:30 +0100

    to change the text in registration form

---
+++ src/Application/Sonata/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace  Application\Sonata\UserBundle\Form\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'SonataUserBundle'))
+            ->add('email', 'email', array('label' => 'form.email', 'translation_domain' => 'SonataUserBundle'))
+            ->add('plainPassword', 'repeated', array(
+                'type' => 'password',
+                'options' => array('translation_domain' => 'SonataUserBundle'),
+                'first_options' => array('label' => 'form.password'),
+                'second_options' => array('label' => 'form.password_confirmation'),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Application\Sonata\UserBundle\Entity\User',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'application_sonata_user_registration';
+    }
+}
